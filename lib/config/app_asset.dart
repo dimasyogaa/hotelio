b254853016ba12ba6ad216e086cf77abe3d8c7af
Yogadimas <yogadimaspambudi@gmail.com>
2024-07-26 15:58:30 +0700

    Intro Page

--- lib/config/app_asset.dart
+++ lib/config/app_asset.dart
@@ -1,15 +1,15 @@
 class AppAsset {
-  static const logo = 'asset/logo.png';
-  static const profile = 'asset/profile.png';
-  static const bgIntro = 'asset/bg_intro.png';
-  static const iconHistory = 'asset/icon_history.png';
-  static const iconMasterCard = 'asset/icon_master_card.png';
-  static const iconNearby = 'asset/icon_nearby.png';
-  static const iconPayment = 'asset/icon_payment.png';
-  static const iconReward = 'asset/icon_reward.png';
-  static const iconSearch = 'asset/icon_search.png';
-  static const iconCoffee = 'asset/icon_coffee.png';
-  static const iconOffice = 'asset/icon_office.png';
-  static const iconStore = 'asset/icon_store.png';
-  static const iconWifi = 'asset/icon_wifi.png';
+  static const logo = 'assets/logo.png';
+  static const profile = 'assets/profile.png';
+  static const bgIntro = 'assets/bg_intro.png';
+  static const iconHistory = 'assets/icon_history.png';
+  static const iconMasterCard = 'assets/icon_master_card.png';
+  static const iconNearby = 'assets/icon_nearby.png';
+  static const iconPayment = 'assets/icon_payment.png';
+  static const iconReward = 'assets/icon_reward.png';
+  static const iconSearch = 'assets/icon_search.png';
+  static const iconCoffee = 'assets/icon_coffee.png';
+  static const iconOffice = 'assets/icon_office.png';
+  static const iconStore = 'assets/icon_store.png';
+  static const iconWifi = 'assets/icon_wifi.png';
 }
---
+++ lib/widget/button_custom.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+import '../config/app_color.dart';
+
+class ButtonCustom extends StatelessWidget {
+  const ButtonCustom(
+      {super.key, required this.label, required this.onTap, this.isExpand});
+
+  final String label;
+  final Function onTap;
+  final bool? isExpand;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 50,
+      child: Stack(
+        children: [
+          Align(
+            alignment: const Alignment(0, 0.7),
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: const [
+                    BoxShadow(
+                        color: AppColor.primary,
+                        offset: Offset(0, 5),
+                        blurRadius: 20)
+                  ]),
+              width: isExpand == null
+                  ? null
+                  : isExpand!
+                      ? double.infinity
+                      : null,
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10),
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              child: Text(label),
+            ),
+          ),
+          Align(
+            child: Material(
+              color: AppColor.primary,
+              borderRadius: BorderRadius.circular(20),
+              child: InkWell(
+                borderRadius: BorderRadius.circular(20),
+                onTap: () => onTap(),
+                child: Container(
+                  width: isExpand == null
+                      ? null
+                      : isExpand!
+                          ? double.infinity
+                          : null,
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 36.0, vertical: 12),
+                  child: const Text(
+                    "Get Started",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w900),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
